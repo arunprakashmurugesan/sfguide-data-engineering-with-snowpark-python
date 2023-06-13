5abeab90b675392cd3e6e23c8543c27f894342e4
arunprakashmurugesan <136509150+arunprakashmurugesan@users.noreply.github.com>
2023-06-13 21:18:04 +0000

    test

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -2,7 +2,7 @@
 # Hands-On Lab: Data Engineering with Snowpark
 # Script:       05_fahrenheit_to_celsius_udf/app.py
 # Author:       Jeremiah Hansen, Caleb Baechtold
-# Last Updated: 1/9/2023
+# Last Updated: 1/9/2023 test
 #------------------------------------------------------------------------------
 
 # SNOWFLAKE ADVANTAGE: Snowpark Python programmability
